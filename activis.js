c30171b6198c733859a4f2d60b361e00ee825711
habotv <117486074+habotv@users.noreply.github.com>
2022-12-27 09:25:44 +0700

    Update activis.js

--- activis.js
+++ activis.js
@@ -679,7 +679,7 @@ if (preloader) {
     });
 }
 
-<script type="text/javascript">var _Hasync= _Hasync|| [];
+var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4727948,4,511,95,18,00000000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
@@ -687,4 +687,5 @@ _Hasync.push(['Histats.track_hits', '']);
 var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
+})();
+
